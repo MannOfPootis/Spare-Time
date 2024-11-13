2e922f7719d7ac132b35e1098b5ffefd4dc2561f
MannOfPootis <99479995+MannOfPootis@users.noreply.github.com>
2024-11-13 17:38:13 +0100

    adding differnt ways od delivering force
    now I have euler():
    just does eulers method and has a constant force over time
    
    then I have eulerTrunc():
    does euler but stops exactly when the collision is done(all the momentum is spent) to save on processing power
    
    and eulerTruncLin()
    this one delivers the force linerly with time so the force isnt constanet but F=ct where c is an arbitrary constant

--- oscilator/main.cpp
+++ oscilator/main.cpp
@@ -9,23 +9,24 @@ double momentum = 1000;//giblana količina istrelka
 double given = 0;//gibalna količina klade
 //double series[10000]; nepotrebno
 double     dt = 0.01;
+static double m, x, v, a, t, tmax, k;
 
 FILE *datek;
 
-double collison(double force) {//funkcija ki poda konstantno silo
+double collison(double force) {
+    if(given>=momentum){return 0;}//funkcija ki poda konstantno silo
     given += force;
     return force;
 
 }//vem da to nebi raabila biti metoda ampapak s tem lažje nakazujem da je lahko funkcija
 double collisonlinear(double t) {
     //constanmt f
-    double c=10;
-    given += c*t;
-    return c*t;
+    if(given>=momentum){return 0;}
+    given += t;
+    return t;
 
 }
-
-double euler(double force) {
+double eulerTruncLin(int i) {
     double m, x, v, a, t, tmax, k;
     m = 1;                               //določam vrednosti
     x = 0;
@@ -34,6 +35,31 @@ double euler(double force) {
     t = 0;
     k = 1;
     for (t = 0; given < momentum; t += dt) {
+        x += v * dt;
+        v += a * dt;
+        a = (collison(momentum/i*t) - x * k) / m;
+        //series[dex] = x;
+        //std::cout << x << "\n";
+    }
+    x += v * dt;//dodatene tick hitrosti zato da za zelo majhne trke
+    v += a * dt;
+    std::cout<<k*x*x/2+m*v*v/2;
+    double W=    k*x*x/2+m*v*v/2;
+    double w=1;/* w= sqrt(k/m) sam si bom življenje prišoparu računalniku ker sta oba dva 1 */
+    //std::cout<<m*v+k*sqrt(2*W/k)*(1-cos(t))<<", "; //ocna za konservacijo gibalne količine
+    return k*x*x/2+m*v*v/2;
+
+}
+double euler(double force,double tmax) {
+    double m, x, v, a, t, k;
+    m = 1;                               //določam vrednosti
+    x = 0;
+    v = 0;
+    a = 0;
+    t = 0;
+    k = 1;
+   // tmax=40;
+    for (t = 0; t<tmax; t += dt) {
         x += v * dt;
         v += a * dt;
         a = (collison(force) - x * k) / m;
@@ -49,16 +75,48 @@ double euler(double force) {
     return k*x*x/2+m*v*v/2;
 
 }
+
+double eulerTrunc(int i) {
+   /* m = 1;                               //določam vrednosti
+    x = 0;
+    v = 0;
+    a = 0;
+    t = 0;
+    k = 1;*/
+    for (t = 0; given < momentum; t += dt) {
+        x += v * dt;
+        v += a * dt;
+        a = (collison(momentum/i) - x * k) / m;
+        //series[dex] = x;
+        //std::cout << x << "\n";
+    }
+    x += v * dt;//dodatene tick hitrosti zato da za zelo majhne trke
+    v += a * dt;
+    std::cout<<k*x*x/2+m*v*v/2;
+    double W=    k*x*x/2+m*v*v/2;
+    double w=1;/* w= sqrt(k/m) sam si bom življenje prišoparu računalniku ker sta oba dva 1 */
+    //std::cout<<m*v+k*sqrt(2*W/k)*(1-cos(t))<<", "; //ocna za konservacijo gibalne količine
+    return k*x*x/2+m*v*v/2;
+
+}
 void makeGraph(char* myString,double chaing){
+
     datek=fopen(myString,"w");
     for(double i=1;i<2000;i+=1){
-        fprintf(datek,"%E\n ",   euler(momentum/i));
+        m = 1;                               //določam vrednosti
+        x = 0;
+        v = 0;
+        a = 0;
+        t = 0;
+        k = 0.01;
+        fprintf(datek,"%E\n ",   eulerTrunc(i));
         given=0;
     }
     fclose(datek);
 }
 
 int main() {
+
     makeGraph("data.txt",10);//metoda ki naredi datoteko
   // std::cout<<1.0/(int)2;
   //std::cout<<cos(3.14);
