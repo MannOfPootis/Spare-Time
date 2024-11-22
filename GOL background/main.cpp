e55318b836879a2393650d35903eb4e5457c4b75
MannOfPootis <99479995+MannOfPootis@users.noreply.github.com>
2024-11-22 11:51:05 +0100

    Game of life Background works now
    It works I finished it fel free to use it

--- GOL background/main.cpp
+++ GOL background/main.cpp
@@ -9,13 +9,21 @@
 const std::string IMAGE_PATH = "/home/doominik/Downloads/image.psd(1).jpg";
 const int width = 1920;
 const int height = 1080;
+const int margin =1;
+const int size=5;
+const int sizex=width*0.75/(margin*size);
+const int sizey=height*0.75/(margin*size);
+bool spacepl[sizex][sizey] = {{false}};
+bool spaceTemppl[sizex][sizey] = {{true}};
+bool (*space)[sizey] = spacepl;
+bool (*spaceTemp)[sizey] = spaceTemppl;
 void createImage(const std::string& filepath) {
     // Dimensions of the image
 
 
     // Get the current time
-    std::time_t now = std::time(nullptr);
-    std::string timestamp = std::ctime(&now);
+ //   std::time_t now = std::time(nullptr);
+   // std::string timestamp = std::ctime(&now);
 
     // Create a Cairo surface and context
     cairo_surface_t* surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
@@ -24,12 +32,23 @@ void createImage(const std::string& filepath) {
     // Draw background
     cairo_set_source_rgb(cr, 1, 0.18, 0.20); // Dark gray
     cairo_paint(cr);
-
-    // Draw text
     cairo_set_source_rgb(cr,1,1,1);
+    for(int i =0;i<sizex;i++){
+           for(int j=0;j<sizey;j++){
+            if(space[i][j]){
+                cairo_rectangle(cr,0.05*width+(size+margin)*i,0.05*height+(size+margin)*j,size,size);
 
-    cairo_rectangle(cr,100,100,10,10);
-    cairo_fill(cr);
+                cairo_fill(cr);
+
+            }
+
+
+           }
+       }
+    // Draw text
+
+
+
   //  cairo_move_to(cr, 100, 200);
 
     cairo_surface_write_to_png(surface, filepath.c_str());
@@ -44,11 +63,41 @@ void setBackground(const std::string& filepath) {
     std::string command = "gsettings set org.gnome.desktop.background picture-uri 'file://" + filepath + "'";
     std::system(command.c_str());
 }
+void GOLplane()
+{
+    for(int i=0;i<sizex;i++){
+        for(int j =0;j<sizey;j++){
+            int box=0;
+            for(int k=-1;k<=1;k++){
+                for(int u=-1;u<=1;u++){
+                    box+=(int)space[(i+k)%sizex][(j+u)%sizey];
+                }
+            }
+            spaceTemp[i][j]= (box==3)||(box==4)&&(space[i][j]);
+        }
+    }
+
+    std::swap(space, spaceTemp);
+}
+
+
 
 int main() {
-    const int sleep_interval = 1; // Time interval in seconds
+    const int sleep_interval = 5; // Time interval in seconds
     int i =0;
-    while (i<10) {
+    for(int i =0;i<sizex;i++){
+        for(int j=0;j<sizey;j++){
+            //if(space[i][j]){
+            space[i][j]=(rand()%10)==0;
+
+            //}
+
+
+        }
+    }
+    while (i<1000) {
+        GOLplane();
+
         createImage(IMAGE_PATH);      // Create a new image
         setBackground(IMAGE_PATH);   // Set it as the background
         std::this_thread::sleep_for(std::chrono::seconds(sleep_interval));
